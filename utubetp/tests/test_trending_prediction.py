8efbba881cb6b65394f80c1ccd2218252475d9d6
Ray Wang <wr9802@gmail.com>
2021-12-12 03:30:29 -0800

    Added unittest file for module trending_prediction

--- utubetp/tests/test_trending_prediction.py
+++ utubetp/tests/test_trending_prediction.py
@@ -3,48 +3,48 @@ Tests for the trending_prediction module
 """
 
 import unittest
+import pandas as pd
 from utubetp.trending_prediction import *
 
 
 
-class TestKnn(unittest.TestCase):
+class TestTrendingPred(unittest.TestCase):
+    """Class for unittest."""
+    df = pd.read_csv('../../data/US_youtube_trending_data.csv')
 
     def test_smoke(self):
-        """
-        Simple smoke test to make sure function runs.
-        """
-        
+        """Smoke test to ensure the function running."""
+        df_cleaned = df_cleaning(df)
+        get_video_ids(df_cleaned)
+        output_video_dict(df_cleaned)
+        get_current_trending(df_cleaned)
+        get_current_ids(df_cleaned)
+        output_current_dict(df_cleaned)
+        pred_current_dict(df_cleaned)
+        output_prediction(df_cleaned)
         return
 
-    def test_one_shot1(self):
-        """
-        One shot test.
-        """
-        
+    def test_one_shot(self):
+        """One shot test to make sure the cleaned DataFrame has the correct schema"""
+        df_cleaned = df_cleaning(df)
+        assert list(df_cleaned.columns) == ['video_id', 'title', 'categoryId',
+                                            'trending_date', 'view_count']
         return
 
-    def test_one_shot2(self):
+    def test_edge_not_df(self):
         """
-        One shot test.
+        Edge test with wrong type of data input, to see the function can throw a TypeError.
         """
-        
+        df_raw = [1, 2, 3, 4, 5]
+        with self.assertRaises(TypeError):
+            df_cleaning(df_raw)
         return
 
-    def test_one_shot3(self):
+    def test_edge_not_cleaned(self):
         """
-        One shot test3. 
+        Edge test with input of the original DataFrame, to see the function can throw a ValueError.
         """
-        
+        df_cleaned = df
+        with self.assertRaises(ValueError):
+            get_video_ids(df_cleaned)
         return
-
-    def test_edge_n_out_of_range(self):
-        """
-        Edge test to make sure the function throws a ValueError
-        """
-        
-        return
-
-    def test_edge_input_is_not_number(self):
-        """
-        Edge test to make sure the function throws a ValueError
-        """
