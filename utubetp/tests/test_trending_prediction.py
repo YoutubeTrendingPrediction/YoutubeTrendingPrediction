cc65219d4df4c2341e0e42dcf96d5e2374792e03
Ray Wang <wr9802@gmail.com>
2021-12-13 01:22:10 -0800

    Modified test file to avoid ImportError

--- utubetp/tests/test_trending_prediction.py
+++ utubetp/tests/test_trending_prediction.py
@@ -4,16 +4,22 @@ Tests for the trending_prediction module
 
 import unittest
 import pandas as pd
-from utubetp.trending_prediction import *
-
+from utubetp.trending_prediction import df_cleaning
+from utubetp.trending_prediction import get_current_ids
+from utubetp.trending_prediction import get_current_trending
+from utubetp.trending_prediction import get_video_ids
+from utubetp.trending_prediction import output_current_dict
+from utubetp.trending_prediction import output_prediction
+from utubetp.trending_prediction import output_video_dict
+from utubetp.trending_prediction import pred_current_dict
 
 
 class TestTrendingPred(unittest.TestCase):
     """Class for unittest."""
-    df = pd.read_csv('../../data/US_youtube_trending_data.csv')
 
     def test_smoke(self):
         """Smoke test to ensure the function running."""
+        df = pd.read_csv('../../data/US_youtube_trending_data.csv')
         df_cleaned = df_cleaning(df)
         get_video_ids(df_cleaned)
         output_video_dict(df_cleaned)
@@ -26,6 +32,7 @@ class TestTrendingPred(unittest.TestCase):
 
     def test_one_shot(self):
         """One shot test to make sure the cleaned DataFrame has the correct schema"""
+        df = pd.read_csv('../../data/US_youtube_trending_data.csv')
         df_cleaned = df_cleaning(df)
         assert list(df_cleaned.columns) == ['video_id', 'title', 'categoryId',
                                             'trending_date', 'view_count']
@@ -44,6 +51,7 @@ class TestTrendingPred(unittest.TestCase):
         """
         Edge test with input of the original DataFrame, to see the function can throw a ValueError.
         """
+        df = pd.read_csv('../../data/US_youtube_trending_data.csv')
         df_cleaned = df
         with self.assertRaises(ValueError):
             get_video_ids(df_cleaned)
