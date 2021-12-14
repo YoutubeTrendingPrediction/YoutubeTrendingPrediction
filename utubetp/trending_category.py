4bdce46899032dcda2879459d0bdf207d19504cf
Xinran Wang <wangxinransz@163.com>
2021-12-14 10:32:51 -0800

    category module modified

--- utubetp/trending_category.py
+++ utubetp/trending_category.py
@@ -1,6 +1,6 @@
 """
 This module is used to analysis the trending categoriy along with
-the time variation. The time steps can be daily and monthly.
+the time variation. The time steps can be days and months.
 """
 
 import pandas as pd
@@ -11,7 +11,7 @@ df=pd.read_csv('../data/US_youtube_trending_data.csv')
 def timesplit(data):
     """
     This function is used to extract detailed time information, such
-    as hour, year, month and so on, from the original time column.
+    as hours, years, and months, from the original time column.
     Input: The original dataFrame.
     Output: The new dataFrame with added columns in various time steps.
     """
@@ -47,3 +47,22 @@ def splitcategory(df1):
     df5=df4.reset_index()
     df6=df5.drop(['index'],axis=1)
     return df6
+
+
+def detailtimesplit(datagraph):
+    """
+    This function is used to split the aforementioned dataframe into monthly data
+    Input: The previous groupbyed dataframe
+    Ouput: Seperated monthly data in a list
+    """
+    set=[]
+    for i in yearnumber:
+         for j in range(1,13):
+             if i==yearnumber[0] and j>=1 and j<monthnumber[0]:
+                continue
+             elif i==yearnumber[1] and j==12:
+                continue
+             else:
+                data=df6[(df6.year==i)&(df6.month==j)]
+                set.append(data)
+    return set
