715361fc2f822d98e9059c9e6f0d7e4861d56f9c
Xinran Wang <wangxinransz@163.com>
2021-12-14 19:04:26 -0800

    Modified

--- utubetp/tests/test_trending_category.py
+++ utubetp/tests/test_trending_category.py
@@ -9,31 +9,31 @@ from utubetp.trending_category import dataframecombine
 from utubetp.trending_category import splitcategory
 from utubetp.trending_category import detailtimesplit
 
-class TestCategory(unittest.TestCase):
-   
-    def test_smoke(self):
-        """
-        The smoke test makes sure that the function runs well
-        
-        """
-        df_category=pd.read_csv('./data/US_category_id.csv')
-        df=pd.read_csv('./data/US_youtube_trending_data.csv')
-        newdataframe= timesplit(df)
-        preprocessframe=dataframecombine(newdataframe,df_category)
-        outputframe1=splitcategory(preprocessframe)
-        outputframe2=detailtimesplit(outputframe1)
-        return
 
+class TestCategory(unittest.TestCase):
+
+    def test_smoke(self):
+     """
+     The smoke test makes sure that the function runs well
+     """
+     df_category=pd.read_csv('./data/US_category_id.csv')
+     df=pd.read_csv('./data/US_youtube_trending_data.csv')
+     newdataframe= timesplit(df)
+     preprocessframe=dataframecombine(newdataframe,df_category)
+     outputframe1=splitcategory(preprocessframe)
+     outputframe2=detailtimesplit(outputframe1)
+     return
+  
 
     def test_edge(self):
-         """
-         Edge test with wrong type of data input, to see if the function
-         can throw a TypeError
-         """
-        df=pd.read_csv('../data/US_category_id.csv')
-        randomdata=[a,b,c,d,3,4]
-        with self.assertRaises(TypeError):
-            timesplit(df)
-            dataframecombine(randomdata,df)
-            splitcategory(df)
-            detailtimesplit(df)
+    """
+    Edge test with wrong type of data input, to see if the function
+    can throw a TypeError
+    """
+    df=pd.read_csv('../data/US_category_id.csv')
+    randomdata=[a,b,c,d,3,4]
+    with self.assertRaises(TypeError):
+        timesplit(df)
+        dataframecombine(randomdata,df)
+        splitcategory(df)
+        detailtimesplit(df)
